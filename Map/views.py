9488575740b58d1d2b6287e5d41bf0ae6885acdd
Your Name <you@example.com>
2021-01-01 00:09:39 +0900

    try the map

--- Map/views.py
+++ Map/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'Map/post_list.html', {})
+    return render(request, 'Map/sample3.html', {})
