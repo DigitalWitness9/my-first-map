4a471563a16f1e177088fad1fb70a489f9243a1b
Your Name <you@example.com>
2021-01-01 00:04:34 +0900

    Changed the HTML for the site.

--- Map/views.py
+++ Map/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'Map/post_list.html', {})
--- testproject/urls.py
+++ testproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', include('Map.urls')),
     #path('hello/', views.helloworld),   #Test1
 ]
